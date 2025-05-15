29d2fb01fde977f28b13f9ae88c305210124dbd8
Hugo14156 <hugodelasnerie@gmail.com>
2025-05-15 16:25:58 -0400

    Added variable camera position to dud.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manual/Manual.java
@@ -1,72 +1,15 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode.manual;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
 @TeleOp(name="Hugo's Manual", group="Linear OpMode")
-//@Disabled
+@Disabled
 public class Manual extends LinearOpMode {
 
     // Declare OpMode members for each of the 7 motors.
@@ -80,9 +23,9 @@ public class Manual extends LinearOpMode {
     private DcMotor rightLiftDrive = null;
 
     // Declare OpMode members for each of the 2 continuous servos and one standard servo.
-    private CRServo armServo = null;
-    private Servo intakeServo = null;
-    private Servo outtakeServo = null;
+    private Servo armServo = null;
+    private CRServo intakeServo = null;
+    private CRServo outtakeServo = null;
 
     @Override
     public void runOpMode() {
@@ -96,36 +39,35 @@ public class Manual extends LinearOpMode {
         armDrive = hardwareMap.get(DcMotor.class, "arm_drive");
         leftLiftDrive = hardwareMap.get(DcMotor.class, "left_lift_drive");
         rightLiftDrive = hardwareMap.get(DcMotor.class, "right_lift_drive");
-        armServo = hardwareMap.get(CRServo.class, "arm_servo");
-        intakeServo = hardwareMap.get(Servo.class, "intake_servo");
-        outtakeServo = hardwareMap.get(Servo.class, "outtake_servo");
+        armServo = hardwareMap.get(Servo.class, "arm_servo");
+        intakeServo = hardwareMap.get(CRServo.class, "intake_servo");
+        outtakeServo = hardwareMap.get(CRServo.class, "outtake_servo");
 
-        // ########################################################################################
-        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
-        // ########################################################################################
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
-        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
-        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
-        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        armServo.setDirection(DcMotor.Direction.FORWARD);
-        leftLiftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightLiftDrive.setDirection(DcMotor.Direction.REVERSE);
+        armDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftLiftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightLiftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+//        armServo.setDirection(DcMotor.Direction.FORWARD);
 
-        double armEncoderCalibration = armDrive.getCurrentPosition();
-        double leftLiftEncoderCalibration = leftLiftDrive.getCurrentPosition();
-        double rightLiftEncoderCalibration = rightLiftDrive.getCurrentPosition();
-        double armEncoderLimit = 500;
-        double liftEncoderLimit = 1000;
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftLiftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLiftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        int armEncoderCalibration = armDrive.getCurrentPosition();
+        int leftLiftEncoderCalibration = leftLiftDrive.getCurrentPosition();
+        int rightLiftEncoderCalibration = rightLiftDrive.getCurrentPosition();
+        int armEncoderLimit = 500;
+        int liftEncoderLimit = 1000;
 
         // Wait for the game to start (driver presses START)
-        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Status", "Initialized - Waiting for driver to start");
         telemetry.update();
 
         waitForStart();
@@ -139,10 +81,10 @@ public class Manual extends LinearOpMode {
             double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
             double lateral =  gamepad1.left_stick_x;
             double yaw     =  gamepad1.right_stick_x;
-            double armAdjustedEncoder = armDrive.getCurrentPosition() - armEncoderCalibration;
-            double leftLiftAdjustedEncoder = leftLiftDrive.getCurrentPosition() - leftLiftEncoderCalibration;
-            double rightLiftAdjustedEncoder = rightLiftDrive.getCurrentPosition() - rightLiftEncoderCalibration;
-            int armPosition = 5;
+            int armAdjustedEncoder = armDrive.getCurrentPosition() - armEncoderCalibration;
+            int leftLiftAdjustedEncoder = leftLiftDrive.getCurrentPosition() - leftLiftEncoderCalibration;
+            int rightLiftAdjustedEncoder = rightLiftDrive.getCurrentPosition() - rightLiftEncoderCalibration;
+            int armPosition = 0;
             boolean outtake = false;
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
@@ -151,32 +93,32 @@ public class Manual extends LinearOpMode {
             double rightFrontPower = 0;
             double leftBackPower   = 0;
             double rightBackPower  = 0;
-            double armPower = 0;
+            double armPower = -1;
             double leftLiftPower = 0;
             double rightLiftPower = 0;
 
             if (gamepad1.right_bumper) {
-                if (armAdjustedEncoder >= armEncoderLimit || armAdjustedEncoder <=0){
-                    armPower = 0;
-                } else{
+                if (armAdjustedEncoder < armEncoderLimit && armAdjustedEncoder > 0){
                     armPower = gamepad1.left_stick_y;
                 }
             } else if (gamepad1.left_bumper) {
-                if (leftLiftAdjustedEncoder >= liftEncoderLimit || leftLiftAdjustedEncoder <= 0){
-                    leftLiftPower = 0;
-                } else{
+//              Digital stop switches
+                if (leftLiftAdjustedEncoder < liftEncoderLimit && leftLiftAdjustedEncoder > 0){
                     leftLiftPower = gamepad1.left_stick_y;
                 }
-                if (rightLiftAdjustedEncoder >= liftEncoderLimit || rightLiftAdjustedEncoder <= 0){
-                    rightPower = 0;
-                } else{
-                    rightPower = gamepad1.left_stick_y;
+                if (rightLiftAdjustedEncoder < liftEncoderLimit && rightLiftAdjustedEncoder > 0){
+                    rightLiftPower = gamepad1.left_stick_y;
                 }
             } else {
                 leftFrontPower  = axial + lateral + yaw;
                 rightFrontPower = axial - lateral - yaw;
                 leftBackPower   = axial - lateral + yaw;
                 rightBackPower  = axial + lateral - yaw;
+
+                if (Math.abs(leftLiftAdjustedEncoder - rightLiftAdjustedEncoder) >= 10){
+                   leftLiftDrive.setTargetPosition((leftLiftAdjustedEncoder + rightLiftAdjustedEncoder)/2);
+                   rightLiftDrive.setTargetPosition((leftLiftAdjustedEncoder + rightLiftAdjustedEncoder)/2);
+                }
             }
 
             // Normalize the values so no wheel power exceeds 100%
@@ -192,31 +134,31 @@ public class Manual extends LinearOpMode {
                 rightBackPower  /= max;
             }
 
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
+            if (gamepad1.dpad_down && gamepad1.dpad_up){
+                armPosition = 1;
+            } else if (gamepad1.dpad_down){
+                armPosition = 0;
+            } else if (gamepad1.dpad_up){
+                armPosition = 2;
+            }
 
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
-
-            // Send calculated power to wheels
+            intakeServo.setPower(0);
+            if (armPosition == 0){
+                armServo.setPosition(0.2);
+            } else if (armPosition == 1) {
+                armServo.setPosition(0.5);
+            } else if (armPosition == 2) {
+                armServo.setPosition(0.7);
+                intakeServo.setPower(1);
+            }
+            // Send calculated power to motors
             leftFrontDrive.setPower(leftFrontPower);
             rightFrontDrive.setPower(rightFrontPower);
             leftBackDrive.setPower(leftBackPower);
             rightBackDrive.setPower(rightBackPower);
             armDrive.setPower(armPower);
-            leftLiftDrive.setPower(liftPower);
-            rightLiftDrive.setPower(liftPower);
+            leftLiftDrive.setPower(leftLiftPower);
+            rightLiftDrive.setPower(rightLiftPower);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
@@ -224,4 +166,5 @@ public class Manual extends LinearOpMode {
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.update();
         }
-    }}
+    }
+}
